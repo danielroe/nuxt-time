8582d378aca15ad97c3a5df831ca9e4965f843d7
Alex Liu <dsa1314@gmail.com>
2023-02-11 22:32:01 +0800

    refactor: let the props type extend from `Intl.DateTimeFormatOptions` (#5)

--- src/runtime/components/NuxtTime.vue
+++ src/runtime/components/NuxtTime.vue
@@ -1,31 +1,12 @@
 <script setup lang='ts'>
 import { computed, getCurrentInstance, useHead } from '#imports'
 
-const props = defineProps<{
-  locale?: string
+interface NuxtTimeProps extends Intl.DateTimeFormatOptions {
+  locale?: string;
   datetime: string | number | Date
-  localeMatcher?: 'best fit' | 'lookup'
-  weekday?: 'long' | 'short' | 'narrow'
-  era?: 'long' | 'short' | 'narrow'
-  year?: 'numeric' | '2-digit'
-  month?: 'numeric' | '2-digit' | 'long' | 'short' | 'narrow'
-  day?: 'numeric' | '2-digit'
-  hour?: 'numeric' | '2-digit'
-  minute?: 'numeric' | '2-digit'
-  second?: 'numeric' | '2-digit'
-  timeZoneName?: 'short' | 'long' | 'shortOffset' | 'longOffset' | 'shortGeneric' | 'longGeneric'
-  formatMatcher?: 'best fit' | 'basic'
-  hour12?: boolean
-  timeZone?: string
+}
 
-  calendar?: string
-  dayPeriod?: 'narrow' | 'short' | 'long'
-  numberingSystem?: string
-
-  dateStyle?: 'full' | 'long' | 'medium' | 'short'
-  timeStyle?: 'full' | 'long' | 'medium' | 'short'
-  hourCycle?: 'h11' | 'h12' | 'h23' | 'h24'
-}>()
+const props = defineProps<NuxtTimeProps>()
 
 const renderedDate = getCurrentInstance()?.vnode.el?.getAttribute('datetime')
 const locale = getCurrentInstance()?.vnode.el?.getAttribute('data-locale')
