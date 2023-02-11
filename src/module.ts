2b608fe7b71fcd910b8ea68589e4e7d18b1c544f
Harlan Wilton <harlan@harlanzw.com>
2023-02-11 19:17:50 +1100

    fix: avoid script dupes (#2)

--- src/module.ts
+++ src/module.ts
@@ -14,20 +14,6 @@ export default defineNuxtModule<ModuleOptions>({
     const runtimeDir = fileURLToPath(new URL('./runtime', import.meta.url))
     nuxt.options.build.transpile.push(runtimeDir)
 
-    nuxt.options.app.head.script = nuxt.options.app.head.script || []
-    nuxt.options.app.head.script.push({
-      tagPosition: 'bodyClose',
-      innerHTML: `
-        document.querySelectorAll('[data-n-time]').forEach(el => {
-          const date = new Date(parseInt(el.getAttribute('datetime')));
-          const options = Object.fromEntries(el.getAttributeNames().map(name => [name, el.getAttribute(name)]));
-
-          const formatter = new Intl.DateTimeFormat(options.locale, options);
-          el.textContent = formatter.format(date)
-        })
-      `.replace(/\s+/g, ' ')
-    })
-
     // Add <NuxtTime> component
     addComponentsDir({ path: join(runtimeDir, 'components') })
   },
