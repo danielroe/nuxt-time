153c54e4bc0a7fb54a992542ac5199bc46e49780
Daniel Roe <daniel@roe.dev>
2023-02-12 15:56:21 +0100

    refactor: simplify somewhat

--- src/runtime/components/NuxtTime.vue
+++ src/runtime/components/NuxtTime.vue
@@ -38,30 +38,7 @@ const date = computed(() => {
   return new Date(props.datetime)
 })
 
-const useLocale = computed(() => locale ?? props.locale)
-const formatter = computed(() => {
-  return new Intl.DateTimeFormat(useLocale.value, {
-    localeMatcher: props.localeMatcher,
-    weekday: props.weekday,
-    era: props.era,
-    year: props.year,
-    month: props.month,
-    day: props.day,
-    hour: props.hour,
-    minute: props.minute,
-    second: props.second,
-    timeZoneName: props.timeZoneName,
-    formatMatcher: props.formatMatcher,
-    hour12: props.hour12,
-    timeZone: props.timeZone,
-    calendar: props.calendar,
-    dayPeriod: props.dayPeriod,
-    numberingSystem: props.numberingSystem,
-    dateStyle: props.dateStyle,
-    timeStyle: props.timeStyle,
-    hourCycle: props.hourCycle,
-  })
-})
+const formatter = computed(() => new Intl.DateTimeFormat(locale ?? props.locale, props))
 const formattedDate = computed(() => formatter.value.format(date.value))
 const isoDate = computed(() => date.value.toISOString())
 
@@ -70,7 +47,7 @@ const dataset: Record<string, any> = {}
 if (process.server) {
   for (const prop in props) {
     if (prop !== 'datetime') {
-      dataset[`data-${prop}`] = (props as any)?.[prop]
+      dataset[`data-${prop}`] = props?.[prop as keyof typeof props]
     }
   }
   useHead({
