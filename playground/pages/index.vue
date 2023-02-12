8f1264388d0992fdd919a6a87881abd76c7cadae
Daniel Roe <daniel@roe.dev>
2023-02-12 02:12:22 +0100

    test: add fixed locale in server

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -11,6 +11,6 @@ const locale = process.server ? 'en-US' : 'en-GB'
     <br>
     <NuxtTime :datetime="new Date()" second="numeric" month="long" day="numeric" />
     <br>
-    <NuxtTime data-test-fixed datetime="2023-02-11T08:24:08.396Z" month="long" day="numeric" />
+    <NuxtTime data-test-fixed datetime="2023-02-11T08:24:08.396Z" month="long" day="numeric" :locale="locale" />
   </div>
 </template>
