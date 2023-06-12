612bb0dd073e0024e7fdc8ca8473e8c95e279c8a
Carles Mitjans <carles.mitjans.coma@gmail.com>
2023-06-12 17:33:52 +0200

    perf: transpile at build time

--- src/module.ts
+++ src/module.ts
@@ -1,3 +1,4 @@
+import { fileURLToPath } from 'url'
 import { defineNuxtModule, createResolver, addComponentsDir } from '@nuxt/kit'
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
@@ -8,8 +9,11 @@ export default defineNuxtModule<ModuleOptions>({
     configKey: 'time',
     name: 'nuxt-time',
   },
-  async setup() {
+  async setup(_options, nuxt) {
     const resolver = createResolver(import.meta.url)
+
+    const runtimeDir = fileURLToPath(new URL('./runtime', import.meta.url))
+    nuxt.options.build.transpile.push(runtimeDir)
 
     // Add <NuxtTime> component
     addComponentsDir({ path: resolver.resolve('./runtime/components') })
