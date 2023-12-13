464e680168fd5abc4d2f66386acdecbbad95859c
Daniel Roe <daniel@roe.dev>
2023-12-13 17:10:02 +0000

    chore: migrate to `@nuxt/test-utils` alpha (#208)

--- test/unit.spec.ts
+++ test/unit.spec.ts
@@ -1,7 +1,7 @@
 // @vitest-environment nuxt
 import { describe, expect, it } from 'vitest'
-import { mountSuspended } from 'nuxt-vitest/utils'
-import NuxtTime from './src/runtime/components/NuxtTime.vue'
+import { mountSuspended } from '@nuxt/test-utils/runtime'
+import NuxtTime from '../src/runtime/components/NuxtTime.vue'
 
 describe('<NuxtTime>', () => {
   it('should not serialise data in the DOM in the client', async () => {
@@ -18,7 +18,7 @@ describe('<NuxtTime>', () => {
       })
     )
     expect(thing.html()).toMatchInlineSnapshot(
-      '"<time data-n-time=\\"\\" datetime=\\"2023-02-11T18:26:41.058Z\\">11 February at 41</time>"'
+      `"<time data-n-time="" datetime="2023-02-11T18:26:41.058Z">11 February at 41</time>"`
     )
   })
 })
