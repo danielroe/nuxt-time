6acfe1b8a3fb34d08412c93cc86de48e8578038b
Daniel Roe <daniel@roe.dev>
2024-03-17 19:35:43 +0100

    perf: minify prehydration script
    resolves https://github.com/danielroe/nuxt-time/issues/3

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,5 @@
-import { defineNuxtModule, createResolver, addComponentsDir } from '@nuxt/kit'
+import { promises as fsp } from 'node:fs'
+import { defineNuxtModule, createResolver, addComponentsDir, addTemplate } from '@nuxt/kit'
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface ModuleOptions {}
@@ -8,10 +9,19 @@ export default defineNuxtModule<ModuleOptions>({
     configKey: 'time',
     name: 'nuxt-time',
   },
-  async setup() {
+  async setup () {
     const resolver = createResolver(import.meta.url)
 
     // Add <NuxtTime> component
     addComponentsDir({ path: resolver.resolve('./runtime/components') })
+
+    const script = await fsp.readFile(resolver.resolve('./script.mjs'), 'utf-8')
+
+    addTemplate({
+      filename: 'nuxt-time-script.mjs',
+      getContents () {
+        return `export default ${JSON.stringify(script.trim())}`
+      }
+    })
   },
 })
--- src/runtime/components/NuxtTime.vue
+++ src/runtime/components/NuxtTime.vue
@@ -1,5 +1,7 @@
 <script setup lang='ts'>
 import { computed, getCurrentInstance, useNuxtApp, useHead } from '#imports'
+// @ts-expect-error virtual file
+import scriptContents from '#build/nuxt-time-script.mjs'
 
 const props = withDefaults(defineProps<{
   locale?: string
@@ -63,26 +65,7 @@ if (process.server) {
       tagPosition: 'bodyClose',
       tagPriority: -20,
       key: 'nuxt-time',
-      innerHTML: `
-        document.querySelectorAll('[data-n-time]').forEach(el => {
-          const toCamelCase = (name, index) => {
-            if (index > 0) { return name[0].toUpperCase() + name.slice(1) };
-            return name;
-          };
-
-          const date = new Date(el.getAttribute('datetime'));
-          const options = {};
-          for (const name of el.getAttributeNames()) {
-            if (name.startsWith('data-')) {
-              const optionName = name.slice(5).split('-').map(toCamelCase).join('');
-              options[optionName] = el.getAttribute(name);
-            }
-          }
-
-          const formatter = new Intl.DateTimeFormat(options.locale, options);
-          el.textContent = formatter.format(date)
-        })
-      `.replace(/\s+/g, ' ')
+      innerHTML: scriptContents
     }]
   })
 }
