5d7bcc138c4586eb614c09fc6f5a4c549c0a019b
Daniel Roe <daniel@roe.dev>
2024-09-17 10:28:59 +0200

    refactor!: use new `onPrehydrate` hook for implementation (#251)

--- src/module.ts
+++ src/module.ts
@@ -1,5 +1,4 @@
-import { promises as fsp } from 'node:fs'
-import { defineNuxtModule, createResolver, addComponentsDir, addTemplate } from '@nuxt/kit'
+import { defineNuxtModule, createResolver, addComponentsDir } from '@nuxt/kit'
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface ModuleOptions {}
@@ -8,20 +7,14 @@ export default defineNuxtModule<ModuleOptions>({
   meta: {
     configKey: 'time',
     name: 'nuxt-time',
+    compatibility: {
+      nuxt: '>=3.12.0',
+    },
   },
   async setup() {
     const resolver = createResolver(import.meta.url)
 
     // Add <NuxtTime> component
     addComponentsDir({ path: resolver.resolve('./runtime/components') })
-
-    const script = await fsp.readFile(resolver.resolve('./script.mjs'), 'utf-8')
-
-    addTemplate({
-      filename: 'nuxt-time-script.mjs',
-      getContents() {
-        return `export default ${JSON.stringify(script.trim())}`
-      },
-    })
   },
 })
